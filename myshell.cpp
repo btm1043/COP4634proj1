26356916527204467701da5aeff615ac16cb643b
btm1043 <39682210+btm1043@users.noreply.github.com>
2019-09-23 09:54:13 -0500

    Update myshell.cpp

--- myshell.cpp
+++ myshell.cpp
@@ -3,28 +3,59 @@
 
 int main(int argc, const char* argv[])
 {	
+	//Char array to compare user command line argument to set mode to Debug
 	char debug[] = "-Debug";
+	
+	//Char array to store user input
+	char buf[256];
 
-
-	//execl("/bin/ls", "ls", "-l", (char*)NULL);
-
+	//Create new parse object
 	Parse p = Parse();
 	while (p.getFirstArg()!=1)
 	{
-		char buf[256];
+		//Command prompt
 		std::cout << "$$$ ";
 		fgets(buf, 256, stdin);
-
+		
+		//Parse user command
 		p.parseInput(buf);
-		if (argc > 1)
-		{
-			if (strcmp(debug, argv[1]) == 0)
+		if (p.getFirstArg() != 1) {
+			
+			//Debug mode selected print parameters
+			if (argc > 1)
 			{
-				p.printParams();
+				if (strcmp(debug, argv[1]) == 0)
+				{
+					p.printParams();
+				}
 			}
+			//fork process
+			pid = fork();
+			
+			//if child 
+			if (pid == 0) {
+				p.execute();
+				exit(1);
+			}
+			else {
+				//if process suppose to run in background
+				if (p.getBackground() != 1)
+				{
+					result = waitpid(pid, &status, 0);
+				}
+			}
+
+			
+			//reassign the input and output back to NULL
+			p.refresh();
+		
 		}
-		//reassign the input and output back to NULL
-		p.refresh();		
+		else {
+			//if quit command wait for children to exit
+			while ((pid = wait(&status)) > 0);
+		}
+		
+				
 	}
 }
 
