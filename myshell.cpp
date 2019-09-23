32ff71d5af90193761559f25a37f5414ef5fa8d9
btm1043 <39682210+btm1043@users.noreply.github.com>
2019-09-23 09:37:35 -0500

    Update myshell.cpp
    Comments

--- myshell.cpp
+++ myshell.cpp
@@ -3,21 +3,26 @@
 
 int main(int argc, const char* argv[])
 {	
+	//Char array to compare user input to for Debug mode
 	char debug[] = "-Debug";
-
-
-	//execl("/bin/ls", "ls", "-l", (char*)NULL);
-
+	
+	//Char array to house user input
+	char buf[256];
+	
+	//Creates Parse object to house parameters
 	Parse p = Parse();
 	while (p.getFirstArg()!=1)
 	{
-		char buf[256];
+		//Gets user input
 		std::cout << "$$$ ";
 		fgets(buf, 256, stdin);
-
+		
 		p.parseInput(buf);
+		
+		//Check if command line argument includes -Debug
 		if (argc > 1)
 		{
+			//If so print paramaters
 			if (strcmp(debug, argv[1]) == 0)
 			{
 				p.printParams();
